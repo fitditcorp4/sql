538ae6ac5b603ef4d136b313831f0d70c136742f
Nkem Omede <admin@peplot.com>
2023-02-07 21:16:12 +0900

    create table

---
+++ first07022023.sql
@@ -0,0 +1,8 @@
+CREATE TABLE covidDeath(
+iso_code VARCHAR(50),continent VARCHAR(50),country VARCHAR(50),
+fdate DATE,npopulation BIGINT,total_cases BIGINT,
+new_cases BIGINT, new_cases_smoothed REAL, total_deaths BIGINT,
+new_deaths BIGINT, new_deaths_smoothed REAL, total_cases_per_million REAL,
+new_cases_per_million REAL, new_cases_smoothed_per_million
+
+);

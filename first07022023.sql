485609802e3b91ec198203e8da849e1b426e915a
Nkem Omede <admin@peplot.com>
2023-02-07 22:23:18 +0900

    Create table

--- first07022023.sql
+++ first07022023.sql
@@ -3,7 +3,10 @@ iso_code VARCHAR(50),continent VARCHAR(50),country VARCHAR(50),
 fdate DATE,npopulation BIGINT,total_cases BIGINT,
 new_cases BIGINT, new_cases_smoothed REAL, total_deaths BIGINT,
 new_deaths BIGINT, new_deaths_smoothed REAL, total_cases_per_million REAL,
-new_cases_per_million REAL, new_cases_smoothed_per_million,
-total_deaths_per_million
-
+new_cases_per_million REAL, new_cases_smoothed_per_million REAL,
+total_deaths_per_million REAL, new_deaths_per_million REAL,new_deaths_smoothed_per_million REAL,
+reproduction_rate REAL, icu_patients BIGINT, icu_patients_per_million REAL,
+hosp_patients BIGINT,hosp_patients_per_million REAL,weekly_icu_admissions BIGINT,
+weekly_icu_admissions_per_million REAL,weekly_hosp_admissions BIGINT, weekly_hosp_admissions_per_million REAL,
+total_tests BIGINT
 );

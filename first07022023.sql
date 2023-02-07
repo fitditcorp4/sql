484b9a8f5259d725aa51dcc811f3c132c056bec4
Nkem Omede <admin@peplot.com>
2023-02-07 21:53:19 +0900

    create_table

--- first07022023.sql
+++ first07022023.sql
@@ -4,5 +4,6 @@ fdate DATE,npopulation BIGINT,total_cases BIGINT,
 new_cases BIGINT, new_cases_smoothed REAL, total_deaths BIGINT,
 new_deaths BIGINT, new_deaths_smoothed REAL, total_cases_per_million REAL,
 new_cases_per_million REAL, new_cases_smoothed_per_million,
+total_deaths_per_million
 
 );
